a0f7e45e670173d21d04234713c14c0999d72b61
Cesar Oliveira <cesarsdoliveira@gmail.com>
2021-04-11 16:00:55 +1000

    feat: persist user authentication

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ import {
 } from '@chakra-ui/react'
 
 import { Logo } from '../components'
-import firebase from '../config/firebase'
+import firebase, { persistenceMode } from '../config/firebase'
 
 const validationSchema = yup.object().shape({
   email: yup.string().email('Enter a valid email address.').required('Valid e-mail is required.'),
@@ -37,6 +37,8 @@ export default function Home() {
     isSubmitting 
   } = useFormik({
     onSubmit: async (values, form) => {
+      firebase.auth().setPersistence(persistenceMode)
+      
       try {
         const user = await firebase.auth().signInWithEmailAndPassword(values.email, values.password)
         console.log(user)  
